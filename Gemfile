81921bba4a17673892241216e25c232bdf4e82aa
Zadera Mikhail <m.zadera@rambler-co.ru>
2016-09-05 00:24:56 +0300

    Seed database

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'pg'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+USERS_COUNT = 10
+COURSES_COUNT = 5
+
+USER_SKILLS = 3
+USER_WANTS = 4
+COURSE_SKILLS = 2
+COURSE_STUDENTS = 5
+
+tags = [
+  'Ruby', 'Rails', 'PostgreSQL', 'Angular', 'Oracle', 'Highload',
+  'Neural networks', 'Machine learning', 'Big data', 'SQL', 'Git',
+  'HTTP', 'Sinatra', 'C++', 'Python', 'RSpec', 'Testing', 'Vim'
+]
+
+USERS_COUNT.times do |n|
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+tags.count.times do |n|
+  Tag.create(skill: tags[n])
+end
+
+COURSES_COUNT.times do |n|
+  Course.create(teacher: User.order("RANDOM()").first)
+end
+
+User.all.each do |user|
+  USER_SKILLS.times do |n|
+    Skill.create(user: user, tag: Tag.order("RANDOM()").first)
+  end
+end
+
+User.all.each do |user|
+  wants = Tag.all.sample(USER_WANTS)
+  wants.count.times do |n|
+    Want.create(user: user, tag: wants[n])
+  end
+end
+
+Course.all.each do |course|
+  skills = Tag.all.sample(COURSE_SKILLS)
+  skills.count.times do |n|
+    CourseSkill.create(course: course, tag: skills[n])
+  end
+
+  students = User.all.sample(COURSE_STUDENTS)
+  students.count.times do |n|
+    CourseStudent.create(course: course, user: students[n])
+  end
+end
