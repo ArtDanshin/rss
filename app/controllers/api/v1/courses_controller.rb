9a9ebbdb12219060a67866dcec362f1d80d5656d
Zadera Mikhail <m.zadera@rambler-co.ru>
2016-09-08 20:48:40 +0300

    Fix user creation

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,13 +1,10 @@
 module Api::V1
   class CoursesController < ApplicationController
-    # TODO: Change to current_user
-    FAKE_USER_ID = 1
-
     private
 
     def process_courses(courses)
       courses.map do |course|
-        course.attributes.merge(skills: course.skills, state: course.state(FAKE_USER_ID), teacher_name: course.teacher.name)
+        course.attributes.merge(skills: course.skills, state: course.state(current_user.id), teacher_name: course.teacher.name)
       end
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     if ldap_user = User.authenticate(params[:login], params[:pass])
       ldap_user = ldap_user.first
-      user = User.find_or_create_by(login: params[:login], first_name: ldap_user.givenname, last_name: ldap_user.sn)
+      user = User.find_or_create_by(login: params[:login], first_name: ldap_user.givenname.first, last_name: ldap_user.sn.first)
       session[:current_user_id] = user.id
     end
 
